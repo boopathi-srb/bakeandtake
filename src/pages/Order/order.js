d7df37e3a99cca9af10265a73e4ef0e891d62512
Rathina Boopathi <rath.1911180@gct.ac.in>
2022-02-15 20:05:24 +0530

    name changes

--- src/pages/Order/order.js
+++ src/pages/Order/order.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import classes from './order.module.css'
 
+var form = document.getElementById('sheetdb-form');
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  fetch(form.action, {
+      method : "POST",
+      body: new FormData(document.getElementById("sheetdb-form")),
+  }).then(
+      response => response.json()
+  ).then((html) => {
+    // you can put any JS code here
+    window.open( '_blank');
 
+  });
+});
 const Order=(props)=> {
 
  
@@ -14,22 +27,22 @@ const Order=(props)=> {
           </p>
          
 
-        <form className={classes.form}  autocomplete="on">
+        <form action="https://sheetdb.io/api/v1/98cwrppbe6qwy" method="post" id="sheetdb-form" className={classes.form}  autocomplete="on">
           <div>
-            <label>Name</label>
-            <input type="text" placeholder='Enter your name' required />
+            <label for="Name">Name</label>
+            <input type="text" placeholder='Enter your name' name="data[Name]" required />
           </div>
           <div>
-            <label>Email</label>
-            <input type="email" placeholder='Enter your Mail-Id' required/>
+            <label for="Email">Email</label>
+            <input type="email" placeholder='Enter your Mail-Id' name="data[Email]" required/>
           </div>
           <div>
-            <label>Contact Number</label>
-            <input type="text" minLength="10" maxLength="10" pattern="[0-9]{10}" placeholder='Enter your Contact number'required />
+            <label for="Number">Contact Number</label>
+            <input type="text" minLength="10" maxLength="10" pattern="[0-9]{10}" placeholder='Enter your Contact number' name="data[Number]" required />
           </div>
           <div>
-           <label>Cake flavour</label>
-            <select className={classes.selection} required>
+           <label for="Cake flavour">Cake flavour</label>
+            <select className={classes.selection} name="data[Cake flavour]" required>
               <option disabled="disabled" selected>Pick your choice!</option>
               <option>Black forest</option>
               <option>White forest</option>
@@ -50,17 +63,17 @@ const Order=(props)=> {
             </select>
           </div>
           <div>
-          <label>Required theme</label>
-            <input type="text" maxLength="10" placeholder="Enter the theme you wish"></input>
+          <label for="Required theme">Required theme<br/>(IF SELECTED THEME CAKE)</label>
+            <input type="text" maxLength="10" placeholder="Enter the theme you wish" name="data[Required theme]"></input>
           </div>
           <div>
-            <label>Upload a sample pic of your theme(IF AVAILABLE) </label>
-            <input type="file"></input>
+            <label for="Sample pic">Upload a sample pic of your theme<br/>(IF AVAILABLE) </label>
+            <input type="file" name="data[Sample pic]"></input>
           </div>
           <div>
-            <label>Quantity</label>
+            <label for="Quantity">Quantity</label>
             <select className={classes.selection} required>
-            <option disabled="disabled" selected>Pick the quantity</option>
+            <option disabled="disabled" name="data[Quantity]" selected>Pick the quantity</option>
              <option>2kg</option>
              <option>1.5kg</option>
              <option>1kg</option>
@@ -68,12 +81,12 @@ const Order=(props)=> {
             </select>
           </div>
           <div>
-            <label>Address</label>
-            <textarea type="text" rows="6" cols="50" placeholder="Enter your correct address" size="30" required/>
+            <label for="Address">Address</label>
+            <textarea type="text" rows="6" cols="50" placeholder="Enter your correct address" size="30" name="data[Address]" required/>
           </div>
           <div>
-            <label>Date and time of delivery</label>
-            <input type="datetime-local" placeholder="Enter the delivery due time" required/>
+            <label for="Delivery due">Date and time of delivery</label>
+            <input type="datetime-local" placeholder="Enter the delivery due time" name="data[Delivery due]" required/>
           </div>
           <div>
             <button color="blue" type='submit'>Order</button>
