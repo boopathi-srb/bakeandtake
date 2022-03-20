266a7bcbad7f7f04b625de5cdd16135afd9fd222
Rathina Boopathi <rath.1911180@gct.ac.in>
2022-03-20 15:16:04 +0530

    name changes

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -8,11 +8,11 @@ function About() {
       <div className={classes.About_wrapper}>
         <div className={classes.contentwrapper}  data-aos="zoom-in" data-aos-once="true"> 
           <h1 className={classes.About_heading}>About</h1>
-          <p className={classes.content}>
+          <article className={classes.content}>
             We are home bakers, offering different kinds of cakes. The speciality of our cakes is they are <strong>Eggless</strong>.<br></br>
             We provide home delivery in and around Kanuvai within 6km 
            radius. Delivery charge depends on the distance.<br></br>We offer customized cakes at extra cost. 
-         </p>
+         </article>
         </div>
         <div  data-aos="zoom-in" data-aos-once="true">
           <img className={classes.img} src={logo} alt='BnT-logo'></img>
